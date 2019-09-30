55b7a09fe4b0de47bf724192d15ff1c08d29803b
Jaqueline Kraemer <jaquelinekraemer@gmail.com>
2019-09-30 13:18:32 -0300

    Comentário

--- Exercícios/PY002.py
+++ Exercícios/PY002.py
@@ -12,6 +12,7 @@ curto_prazo = 0
 livre = 0
 print('='*60)
 print('Organize as suas finanças com o método 50/20/10/20 a partir de agora!')
+#Imprimindo salário
 print('')
 salario_liq = float(input('Informe o valor do seu salário líquido: '))
 gastos = salario_liq * 50 / 100
@@ -20,11 +21,11 @@ curto_prazo = salario_liq * 10 / 100
 livre = salario_liq * 20 / 100
 print('')
 print('Abaixo mostramos o quanto você pode gastar em cada categoria, de acordo com o valor do seu salário líquido!')
-print('{}Valor do salário líquido: {} R${:.2f}'.format(' '*5,' '*10, salario_liq))
-print('{}Para os gastos você deve destinar {} R${:.2f}'.format(' '*5,' '*10, gastos))
-print('{}Para os investimentos a longo prazo você deve destinar {} R${:.2f}'.format(' '*5,' '*10, longo_prazo))
-print('{}Para os investimentos a curto prazo você deve destinar {} R${:.2f}'.format(' '*5,' '*10, curto_prazo))
-print('{}E livre você tem: {} R${}'.format(' '*5,' '*10, livre))
+print('{}Valor do salário líquido: {} R${:8.2f}'.format(' '*5,' '*10, salario_liq))
+print('{}Para os gastos você deve destinar {} R${:8.2f}'.format(' '*5,' '*10, gastos))
+print('{}Para os investimentos a longo prazo você deve destinar {} R${:8.2f}'.format(' '*5,' '*10, longo_prazo))
+print('{}Para os investimentos a curto prazo você deve destinar {} R${:8.2f}'.format(' '*5,' '*10, curto_prazo))
+print('{}E livre você tem: {} R${:8.2f}'.format(' '*5,' '*10, livre))
 print('')
 print('Fim do programa. Volte sempre!')
 print('='*60)
